a225e4c59cc86c9f7dc355f4b3b35fa95b17e777
Rut Birhanu <birhanurut@gmail.com>
2024-09-02 20:14:41 +0300

    removing default ui

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,17 @@
 
 export default function Home() {
   return (
-    <div className="flex ">
-      <aside>nav</aside>
+    <div className="container flex min-h-screen bg-gradient-to-r from-purple-100 to-pink-100">
+      <aside className="flex flex-col">
+        <h2>Dashboard</h2>
+        <ul>
+          <li>Overview</li>
+          <li>Products</li>
+          <li>Transactions</li>
+        </ul>
+      </aside>
       <main>main</main>
+      
    </div>
   );
 }
